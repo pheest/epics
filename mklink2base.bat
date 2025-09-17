d302c0b55f86181abdb993a499fbc76c1a54bba2
Peter Heesterman <pheesterman@epsyl-alcen.com>
2025-09-17 10:05:51 +0100

    Make links without admin privs.

--- mklink2base.bat
+++ mklink2base.bat
@@ -4,6 +4,6 @@ if "%1"=="" goto blank
 mklink /D base %1
 goto end
 :blank
-mklink /D base base-7.0.9
+mklink /D /J base base-7.0.9
 REM mklink /D configure base-7.0.6\configure
 :end
--- support/mklink2asyn.bat
+++ support/mklink2asyn.bat
@@ -2,4 +2,4 @@ rmdir asyn
 
 SET A_V=%1%
 if "%A_V%"=="" SET A_V=-R4-40-1
-mklink /D asyn asyn%A_V%
+mklink /D /J asyn asyn%A_V%
